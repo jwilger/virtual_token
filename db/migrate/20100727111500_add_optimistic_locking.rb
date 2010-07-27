ccd90a04c0b650fe947bff41a4233d38be418ce8
John Wilger <johnwilger@gmail.com>
2010-07-27 11:17:52 -0700

    Add optimistic locking to tokens

---
+++ db/migrate/20100727111500_add_optimistic_locking.rb
@@ -0,0 +1,9 @@
+class AddClaimantToTokens < ActiveRecord::Migration
+  def self.up
+    add_column :tokens, :lock_version, :integer
+  end
+
+  def self.down
+    remove_column :tokens, :lock_version
+  end
+end
