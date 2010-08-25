157a90d4dfdaede3d5dcfee67ae95e6482cd442d
John Wilger <johnwilger@gmail.com>
2010-08-25 13:00:21 -0700

    Update Rails to 3.0.0rc2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '425deff5c91b667fb5849b0eb7c1f6e1154e7fccaaea7dfebd8b02acd8762e9177f1279fc53a8dc6eef579f41326631781dadf1d0a4fdff681f548a57df995eb'
+VirtualToken::Application.config.secret_token = '7862f7991ffeab2325e86e9ef812a6977658264ed76f906a6c4931516640fb53ee19cea1d7383ce61526ec2eff5d56125771bbb1657c61748d47359d9ae2b309'
