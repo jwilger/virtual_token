5f8f68d4bdeaf4315b40f776348ec147839ea68e
John Wilger <johnwilger@gmail.com>
2010-11-20 09:34:21 -0800

    clean up migrations

--- db/migrate/20100710191737_add_claimant_to_tokens.rb
+++
@@ -1,9 +0,0 @@
-class AddClaimantToTokens < ActiveRecord::Migration
-  def self.up
-    add_column :tokens, :claimant, :string
-  end
-
-  def self.down
-    remove_column :tokens, :claimant
-  end
-end
--- db/migrate/20100727111500_add_optimistic_locking.rb
+++
@@ -1,9 +0,0 @@
-class AddOptimisticLocking < ActiveRecord::Migration
-  def self.up
-    add_column :tokens, :lock_version, :integer
-  end
-
-  def self.down
-    remove_column :tokens, :lock_version
-  end
-end
